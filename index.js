818e98e75998209cbc90f6933abd584aea7532c3
Bharat Jain <bharatjain7723@gmail.com>
2021-05-26 10:17:33 +0530

    change in commander syntax

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-var program = require('commander');
+const {program} = require('commander');
 
 const {createMongoConnection} = require('./DB/Mongo/mongoose');
 
@@ -22,17 +22,20 @@ program
     .version(require('./package.json')['version'])
     .option('-d, --debug', 'run in debug mode')
     .option('-p, --port [value]', 'specify the port number')
-    .parse(process.argv)
 
-if((!program.port) || program.port === "") {
+program.parse(process.argv);
+
+const options = program.opts();
+
+if((!options.port) || options.port == "") {
     console.log("Please provide the port number");
     console.log("Syntax: node --port <port number>");
     return;
 }
 
-if(program.debug) mode = "debug";
+if(options.debug) mode = "debug";
 
-const port = program.port;
+const port = options.port || 3000; 
 
 const app = express();
 
